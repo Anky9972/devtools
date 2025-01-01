3e39ab4834be176b8aa8d63f47b1460cc66cc3e9
Anky9972 <ankygaur9972@gmail.com>
2025-01-01 20:45:05 +0530

    basics

--- src/app/calculators/scientific/page.jsx
+++ src/app/calculators/scientific/page.jsx
@@ -1,3 +1,4 @@
+"use client"
 import React, { useState } from 'react';
 import { Button } from "@/components/ui/button";
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
